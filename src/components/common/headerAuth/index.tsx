3411f783e61d11b1ec138f7a0eb85bae1f475e9f
Samuel Condack <samuelcondackxz@gmail.com>
2024-05-08 12:46:33 -0300

    add: Search Method, front communication, search page - Search Page

--- src/components/common/headerAuth/index.tsx
+++ src/components/common/headerAuth/index.tsx
@@ -2,7 +2,7 @@ import { Container, Form, Input } from "reactstrap";
 import styles from "./styles.module.scss";
 import Link from "next/link";
 import Modal from "react-modal";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import profileService from "@/src/services/profileService";
 
@@ -11,16 +11,29 @@ Modal.setAppElement("#__next");
 const HeaderAuth = function () {
   const router = useRouter();
   const [modalOpen, setModalOpen] = useState(false);
-  const [initials, setInitials] = useState("")
+  const [initials, setInitials] = useState("");
+  const [searchName, setSearchName] = useState("");
 
-  useEffect(() =>{
-    profileService.fetchCurrent().then((user)=>{
-      const firstNameInitial = user.firstName.slice(0, 1)
-      const lastNameInitial = user.lastName.slice(0, 1)
+  const handleSearch = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-      setInitials(firstNameInitial + lastNameInitial)
-    })
-  })
+    router.push(`search?name=${searchName}`);
+    setSearchName("");
+  };
+
+  const handleSearchClick = () => {
+    router.push(`search?name=${searchName}`);
+    setSearchName("");
+  };
+
+  useEffect(() => {
+    profileService.fetchCurrent().then((user) => {
+      const firstNameInitial = user.firstName.slice(0, 1);
+      const lastNameInitial = user.lastName.slice(0, 1);
+
+      setInitials(firstNameInitial + lastNameInitial);
+    });
+  });
 
   const handleOpenModal = () => {
     setModalOpen(true);
@@ -45,18 +58,23 @@ const HeaderAuth = function () {
           />
         </Link>
         <div className="d-flex align-items-center">
-          <Form>
+          <Form onSubmit={handleSearch}>
             <Input
               name="search"
               type="search"
               placeholder="Search"
               className={styles.input}
+              value={searchName}
+              onChange={(event) => {
+                setSearchName(event.currentTarget.value.toLowerCase());
+              }}
             ></Input>
           </Form>
           <img
             src="/homeAuth/iconSearch.svg"
             alt="lupaHeader"
             className={styles.searchImg}
+            onClick={handleSearchClick}
           />
           <p className={styles.userProfile} onClick={handleOpenModal}>
             {initials}
